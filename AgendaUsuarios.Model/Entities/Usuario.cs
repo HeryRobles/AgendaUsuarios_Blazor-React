f1deb195158b5e98c74ed76624bad6dafd9106e5
Hery Robles <rahe316426@gmail.com>
2025-03-15 00:37:41 -0700

    Nombre de los campos

--- AgendaUsuarios.Model/Entities/Usuario.cs
+++ AgendaUsuarios.Model/Entities/Usuario.cs
@@ -1,11 +1,21 @@
-﻿namespace AgendaUsuarios.Model.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendaUsuarios.Model.Entities
 {
     public class Usuario
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre completo es obligatorio.")]
+
         public string Nombre { get; set; }
+        [Required(ErrorMessage = "El Apellido es obligatorio.")]
         public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El Email es obligatorio.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "El Telefono es obligatorio.")]
         public string Telefono { get; set; }
     }
 }
